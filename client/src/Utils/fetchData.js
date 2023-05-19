affdfc723055f95087dd8994dcfae9bb1013d025
Anjali <anjalidelhi1509@gmail.com>
2023-05-20 00:20:24 +0530

    key changed

--- client/src/Utils/fetchData.js
+++ client/src/Utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions= {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': "0dc13c68cbmshfef3ebbf007320bp189553jsn900a0a651b20",
+      'X-RapidAPI-Key': "f6580a9334msh95774069eaabd85p1e46aajsn8e1f3f3a10a6",
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
@@ -9,7 +9,7 @@ export const exerciseOptions= {
  export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '0dc13c68cbmshfef3ebbf007320bp189553jsn900a0a651b20',
+      'X-RapidAPI-Key': 'f6580a9334msh95774069eaabd85p1e46aajsn8e1f3f3a10a6',
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
